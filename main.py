28bb9c586949cabf2656e5808d5a01822a205f1a
KrystianCz <krystianczapla2000@gmail.com>
2024-04-29 00:32:34 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+def oblicz_h_index(id_artykulow, cytowania):
+    """
+    Funkcja oblicza indeks Hirscha dla danych cytowań artykułów.
+    :param id_artykulow: Lista identyfikatorów artykułów
+    :param cytowania: Lista liczb cytowań odpowiadających artykułom
+    :return: Indeks Hirscha (h-index)
+    """
+    # Tworzenie listy par (id artykułu, liczba cytowań) i sortowanie jej według liczby cytowań malejąco
+    pary = list(zip(id_artykulow, cytowania))
+    pary.sort(key=lambda x: x[1], reverse=True)
+
+    # Obliczanie h-index
+    h_index = 0
+    for i, (_, cyt) in enumerate(pary):
+        if i < cyt:
+            h_index += 1
+        else:
+            break
+
+    return h_index
+
+
+def ewaluacja_jednostki(dane_autorow):
+    """
+    Funkcja zwraca wykaz artykułów zaklasyfikowanych do ewaluacji jednostki oraz oblicza wynik ewaluacji.
+
+    :param dane_autorow: Lista zawierająca dane dla każdego autora w formacie:
+                         [(lista_id_artykulow, lista_punktow), ...]
+    :return: (lista_artykulow_do_ewaluacji, wynik_ewaluacji)
+    """
+    # Słownik do przechowywania sumy punktów dla każdego artykułu
+    punkty_artykulow = {}
+
+    # Przetwarzanie danych każdego autora
+    for id_artykulow, punkty in dane_autorow:
+        for id_art, pkt in zip(id_artykulow, punkty):
+            if id_art in punkty_artykulow:
+                punkty_artykulow[id_art] += pkt
+            else:
+                punkty_artykulow[id_art] = pkt
+
+    # Lista artykułów do ewaluacji (wszystkie artykuły)
+    artykuly_do_ewaluacji = list(punkty_artykulow.keys())
+
+    # Wynik ewaluacji (suma punktów wszystkich artykułów)
+    wynik_ewaluacji = sum(punkty_artykulow.values())
+
+    return artykuly_do_ewaluacji, wynik_ewaluacji
