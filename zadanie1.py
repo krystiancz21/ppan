57e8bcf323e8c57406286a394f8ddaa2d4938d2c
KrystianCz <krystianczapla2000@gmail.com>
2024-05-10 22:47:48 +0200

    maybe final ppan task

--- zadanie1.py
+++ zadanie1.py
@@ -1,4 +1,4 @@
-def oblicz_h_index(quotations):
+def simple_count_hirsch_index(quotations):
     quotations.sort(reverse=True)
 
     # Obliczanie h-index
@@ -12,9 +12,28 @@ def oblicz_h_index(quotations):
     return h_index
 
 
-h_idx1 = [5,3,1]
-h_idx2 = [22,13,76,54,2,6,31,3,1]
+def count_hirsch_index(list_citation):
+    list_citation.sort(reverse=True)
+    # Calculation h-index
+    h_index = sum(1 for i, cyt in enumerate(list_citation) if i < cyt)
+    return h_index
 
-print(oblicz_h_index(h_idx1))
 
-print(oblicz_h_index(h_idx2))
+# Test
+h_idx1 = [5, 3, 1]
+print(simple_count_hirsch_index(h_idx1))
+
+h_idx2 = [22, 13, 76, 54, 2, 6, 31, 3, 1]
+print(simple_count_hirsch_index(h_idx2))
+print(count_hirsch_index(h_idx2))
+
+articles_number = int(input("Number of articles: "))
+
+articles = []
+for i in range(articles_number):
+    item = int(input(f"Give number of cites for {i+1} article: "))
+    articles.append(item)
+
+result = count_hirsch_index(articles)
+
+print("Author's Hirsch index: ", result)
--- zadanie2v3.py
+++ zadanie2v3.py
@@ -1,3 +1,10 @@
+def test_wrapper(func):
+    def wrapper():
+        print("== Only test ===")
+        func()
+        print("=== end test ===")
+    return wrapper
+
 def sort_articles(dane_autorow):
     sort_data = []
     for id_article, points in dane_autorow:
@@ -42,17 +49,51 @@ def ewaluacja_jednostki(dane_autorow):
 
     return lista_artykulow_do_ewaluacji, wynik_ewaluacji
 
+def wczytaj_dane():
+    dane_autorow = []
+    liczba_autorow = int(input("Podaj liczbę autorów: "))
+    for i in range(liczba_autorow):
+        print(f"--- Autor {i+1} ---")
+        print("Zostaw puste pola, jeśli dany autor nie posiada artykułów")
+        id_artykulow_input = input("Podaj ID artykułów oddzielone przecinkami: ")
+        punkty_input = input("Podaj punkty dla każdego artykułu oddzielone przecinkami: ")
 
-dane_autorow2 = [
-    (['Art1', 'Art2', 'Art3', 'Art4'], [15, 20, 5, 10]),
-    (['Art5', 'Art6'], [30, 40]),
-    (['Art7'], [50]),
-    ([], [])
-]
+        if id_artykulow_input.strip() and punkty_input.strip():
+            id_artykulow = id_artykulow_input.split(',')
+            punkty = list(map(int, punkty_input.split(',')))
+        else:
+            id_artykulow = []
+            punkty = []
 
-# Wywołanie funkcji
-artykuly_do_ewaluacji, wynik_ewaluacji = ewaluacja_jednostki(dane_autorow2)
+        dane_autorow.append((id_artykulow, punkty))
+    return dane_autorow
+
+@test_wrapper
+def testing():
+    dane_autorow2 = [
+        (['Art1', 'Art2', 'Art3', 'Art4'], [15, 20, 5, 10]),
+        (['Art5', 'Art6'], [30, 40]),
+        (['Art7'], [50]),
+        ([], [])
+    ]
+
+    # Wywołanie funkcji
+    artykuly_do_ewaluacji, wynik_ewaluacji = ewaluacja_jednostki(dane_autorow2)
+
+    # Wyświetlenie wyników
+    print("Artykuły do ewaluacji:", artykuly_do_ewaluacji)
+    print("Wynik ewaluacji:", wynik_ewaluacji)
+
+
+# only test
+testing()
+
+# Wywołanie funkcji wczytania danych i ewaluacji
+dane_autorow = wczytaj_dane()
+artykuly_do_ewaluacji, wynik_ewaluacji = ewaluacja_jednostki(dane_autorow)
 
 # Wyświetlenie wyników
 print("Artykuły do ewaluacji:", artykuly_do_ewaluacji)
 print("Wynik ewaluacji:", wynik_ewaluacji)
+
+
